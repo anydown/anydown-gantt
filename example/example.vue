f381de227e6d47b8fc893bc2f57545894ecb170e
hashrock <hashedrock@gmail.com>
2018-03-27 02:02:34 +0900

    initial commit

---
+++ example/example.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+
+    <textarea v-model="input"></textarea>
+    </div>
+    <div>
+    <gantt :input="input" @change="update"></gantt>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Gantt from "../gantt.vue";
+export default {
+  data() {
+    return {
+      input: "タスクA 2018-03-30 2018-04-02"
+    };
+  },
+  methods: {
+    update(value) {
+      this.input = value;
+    }
+  },
+  components: {
+    Gantt
+  }
+};
+</script>
+
+<style>
+textarea {
+  width: 100%;
+  height: 10rem;
+}
+</style>
